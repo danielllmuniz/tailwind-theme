feb8f2bcadd31424b9b9a87c013f687b9703a4a9
Daniel Muniz <munidaniel@gmail.com>
2023-08-26 17:44:53 -0300

    upload avatar

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -11,12 +11,18 @@ const config: Config = {
       gridTemplateColumns: {
         app: 'minmax(18rem, 20rem) 1fr',
         profile: 'max-content 1fr min-content',
+        form: 'minmax(7.5rem, 17.5rem) minmax(25rem, 1fr) minmax(0, 15rem)',
+      },
+      borderWidth: {
+        6: '6px',
       },
       maxWidth: {
         app: '700px',
       },
       colors: {
-        daniel: '#1a202c',
+        violet: {
+          25: '#fcfaff',
+        },
       },
     },
   },
