aa01947844fa685c4114e0e082789eb889e83658
Daniel Muniz <munidaniel@gmail.com>
2023-08-26 12:02:17 -0300

    first commit

---
+++ src/app/components/Sidebar/Profile.tsx
@@ -0,0 +1,23 @@
+import { LogOut } from 'lucide-react'
+
+export function Profile() {
+  return (
+    <div className="grid grid-cols-profile items-center gap-3">
+      <img
+        src="https://github.com/danielllmuniz.png"
+        className="h-10 w-10 rounded-full"
+      />
+      <div className="flex flex-1 flex-col truncate">
+        <span className="text-sm font-semibold text-zinc-700">
+          Daniel Muniz
+        </span>
+        <span className="truncate text-sm text-zinc-500">
+          munidaniel@gmail.com
+        </span>
+      </div>
+      <button type="button" className="ml-auto p-2  hover:bg-zinc-50">
+        <LogOut className="h-5 w-5 text-zinc-500" />
+      </button>
+    </div>
+  )
+}
---
+++ tailwind.config.ts
@@ -0,0 +1,25 @@
+import type { Config } from 'tailwindcss'
+
+const config: Config = {
+  content: [
+    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+  ],
+  theme: {
+    extend: {
+      gridTemplateColumns: {
+        app: 'minmax(18rem, 20rem) 1fr',
+        profile: 'max-content 1fr min-content',
+      },
+      maxWidth: {
+        app: '700px',
+      },
+      colors: {
+        daniel: '#1a202c',
+      },
+    },
+  },
+  plugins: [],
+}
+export default config
