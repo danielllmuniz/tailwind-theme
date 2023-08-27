dd92b3f78c82190cfaa076d395b9497e53c623e3
Daniel Muniz <munidaniel@gmail.com>
2023-08-27 16:37:50 -0300

    implementing accessibility

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -172,7 +172,7 @@ export default function Home() {
               <textarea
                 id="bio"
                 defaultValue="I`m a product designer"
-                className="min-h-[120px] w-full resize-y rounded-lg border border-zinc-300 px-3 py-2 shadow-sm"
+                className="min-h-[120px] w-full resize-y rounded-lg border border-zinc-300 px-3 py-2 shadow-sm outline-none focus:border-violet-300 focus:ring-4 focus:ring-violet-100"
               />
             </div>
           </div>
